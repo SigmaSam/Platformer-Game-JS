3f18e36d92af578a12076c3d5dbc97203353f479
samuel <samuelalmaoherrera@gmail.com>
2021-03-31 03:03:10 -0300

    Generated Initial Template.

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,11 @@
+import 'phaser';
+ 
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+  
+  create () {
+    this.add.image(400, 300, 'logo');
+  }
+};
